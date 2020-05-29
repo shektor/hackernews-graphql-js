d4106dc23c5b1ef038bc8ae61496e740f481c016
shektor <s.hektor@gmail.com>
2020-05-29 17:35:49 +0100

    Add resolver for post mutation

--- src/index.js
+++ src/index.js
@@ -6,10 +6,23 @@ let links = [{
     description: 'Fullstack tutorial for GraphQL'
 }]
 
+let idCount = links.length
+
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
         feed: () => links,
+    },
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+            return link
+        }
     },
     Link: {
         id: (parent) => parent.id,
